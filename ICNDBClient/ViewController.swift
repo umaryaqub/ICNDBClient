636f235f127396fad6fa9b1f90cb1a076d0523f2
Umar Yaqub <yaqub_umar@yahoo.co.uk>
2018-12-20 10:51:11 +0000

    Initial Commit

---
+++ ICNDBClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ICNDBClient
+//
+//  Created by Umar Yaqub on 20/12/2018.
+//  Copyright © 2018 Umar Yaqub. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
